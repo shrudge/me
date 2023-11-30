a6423e2308184c87bfd09eb9bc993a893ae71d80
Meet Balani <74920401+shrudge@users.noreply.github.com>
2023-11-30 23:27:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-window.addEventListener("DOMContentLoaded", function() {
+function toggleMenu() {
+  const menu = document.querySelector('.menu');
+  const ham = document.querySelector('.hamRotate180');
+  const refreshButton = document.getElementById('refreshButton');
+
+
+  ham.classList.toggle('active');
+  menu.style.left = ham.classList.contains('active') ? '0' : '-50vh';
+  if (!ham.classList.contains('active')) {
+    // If menu is closing, remove the fragment identifier and scroll to the top
+    window.history.replaceState({}, document.title, window.location.pathname);
+    window.scrollTo(0, 0);
+}
+}
+window.addEventListener("DOMContentLoaded", function () {
   var refreshButton = document.getElementById("refreshButton");
 
-  refreshButton.addEventListener("click", function(e) {
+  refreshButton.addEventListener("click", function (e) {
     e.preventDefault();
     window.location.reload();
   });
